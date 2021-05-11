94d35fb1b49cb01ef3c0d2f7f114a10644495861
Dennis Dörr <dev@ddoerr.com>
2021-05-11 16:28:20 +0200

    initial commit for functions module

---
+++ src/main/java/com/ddoerr/modules/functions/ModuleInfo.java
@@ -0,0 +1,18 @@
+package com.ddoerr.modules.functions;
+
+/**
+ * Shared information for this module
+ */
+public final class ModuleInfo {
+    
+    /**
+     * Target API version
+     */
+    public static final int API_VERSION = 26;
+
+    /**
+     * Private ctor, no instances 
+     */
+    private ModuleInfo() {}
+
+}
