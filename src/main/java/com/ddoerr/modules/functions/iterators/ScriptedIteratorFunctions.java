960ed19758485980328024a3fb8a627eceda4ad6
Dennis Dörr <dev@ddoerr.com>
2021-05-14 13:19:04 +0200

    added iterator for function names. closes #3

---
+++ src/main/java/com/ddoerr/modules/functions/iterators/ScriptedIteratorFunctions.java
@@ -0,0 +1,42 @@
+package com.ddoerr.modules.functions.iterators;
+
+import java.util.Map;
+
+import com.ddoerr.modules.functions.ModuleInfo;
+
+import net.eq2online.macros.scripting.ScriptedIterator;
+import net.eq2online.macros.scripting.api.APIVersion;
+import net.eq2online.macros.scripting.api.IMacro;
+import net.eq2online.macros.scripting.api.IScriptActionProvider;
+import net.eq2online.macros.scripting.parser.ScriptContext;
+
+@APIVersion(ModuleInfo.API_VERSION)
+public class ScriptedIteratorFunctions extends ScriptedIterator {
+	public ScriptedIteratorFunctions(IScriptActionProvider provider, IMacro macro) {
+		super(provider, macro);
+		
+		Map<String, Object> stateData = macro.getStateData();
+		
+		for (Map.Entry<String, Object> entry : stateData.entrySet()) {
+			if (!entry.getKey().startsWith("fn#")) {
+				continue;
+			}
+			
+			begin();
+			
+			add("FUNCTIONNAME", entry.getKey().substring(3));
+			
+			end();
+		}
+	}
+	
+	public ScriptedIteratorFunctions() {
+		super(null, null);
+	}
+	
+	@Override
+	public void onInit() {
+		ScriptContext.MAIN.getCore().registerIterator("functions", this.getClass());
+	}
+
+}
