1cad403a6abda598ffbb47c9cb4ad35d2323df41
Dennis Dörr <dev@ddoerr.com>
2021-08-31 17:03:20 +0200

    use %% as a placeholder in chains

--- src/main/java/com/ddoerr/modules/functions/actions/ScriptActionChainedCall.java
+++ src/main/java/com/ddoerr/modules/functions/actions/ScriptActionChainedCall.java
@@ -1,11 +1,10 @@
 package com.ddoerr.modules.functions.actions;
 
-import java.util.ArrayList;
+import org.apache.commons.lang3.ArrayUtils;
 
 import com.ddoerr.modules.functions.ModuleInfo;
 import com.ddoerr.modules.functions.VariableHandler;
 import com.ddoerr.modules.functions.parser.ActionParserChainedCall;
-import com.google.common.collect.Lists;
 
 import net.eq2online.macros.scripting.api.APIVersion;
 import net.eq2online.macros.scripting.api.IMacro;
@@ -16,6 +15,8 @@ import net.eq2online.macros.scripting.api.IScriptActionProvider;
 
 @APIVersion(ModuleInfo.API_VERSION)
 public class ScriptActionChainedCall extends ScriptActionCall {	
+	private static final String placeholder = "%%";
+	
 	public ScriptActionChainedCall() {
 		super("chainedcall");
 	}
@@ -27,8 +28,7 @@ public class ScriptActionChainedCall extends ScriptActionCall {
 	}
 	
 	@Override
-	public boolean canExecuteNow(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams,
-			String[] params) {
+	public boolean canExecuteNow(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams, String[] params) {
 		if (instance.getState() != null) {
 			return super.canExecuteNow(provider, macro, instance, rawParams, params);
 		}
@@ -43,24 +43,33 @@ public class ScriptActionChainedCall extends ScriptActionCall {
 			value = chainValue.getString();
 		}
 		
-		ArrayList<String> paramsList = Lists.newArrayList(params);
-		paramsList.add(1, value);
-		String[] chainedParams = paramsList.toArray(new String[0]);
-		
-		int commaIndex = rawParams.indexOf(',');
+		String[] chainedParams;
 		String chainedRawParams;
-		if (commaIndex == -1) {
-			chainedRawParams = rawParams + ',' + value;
-		} else {			
-			chainedRawParams = rawParams.substring(0, commaIndex) + ',' + value + "," + rawParams.substring(commaIndex);
+		if (rawParams.contains(placeholder)) {
+			chainedRawParams = rawParams.replace(placeholder, value);
+			chainedParams = ArrayUtils.clone(params);
+			
+			int index = 0;
+			while ((index = ArrayUtils.indexOf(chainedParams, placeholder)) != -1) {
+				chainedParams[index] = value;
+			}
+		} else {
+			int commaIndex = rawParams.indexOf(',');
+			if (commaIndex == -1) {
+				chainedRawParams = rawParams + ' ' + value;
+			} else {			
+				chainedRawParams = rawParams.substring(0, commaIndex) + ' ' + value + ' ' + rawParams.substring(commaIndex);
+			}
+			
+			chainedParams = ArrayUtils.add(params, 1, value);
 		}
+		
 
 		return super.canExecuteNow(provider, macro, instance, chainedRawParams, chainedParams);
 	}
 	
 	@Override
-	public IReturnValue execute(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams,
-			String[] params) {
+	public IReturnValue execute(IScriptActionProvider provider, IMacro macro, IMacroAction instance, String rawParams, String[] params) {
 		String chainVariable = macro.getState("chain_variable");
 		IReturnValue returnValue = super.execute(provider, macro, instance, rawParams, params);
 		macro.setState("chain_variable", chainVariable);
